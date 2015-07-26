de941d380fccbcc5b3ccc44428c572fe6182b96b
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2015-07-25 22:57:21 -0400

    fades

--- client/main.js
+++ client/main.js
@@ -1,18 +1,25 @@
 Template.main.rendered = function() {
-  this.$('#cbp-af-header').vide('desktop-keyscores');
+  //this.$('#cbp-af-header').vide('desktop-keyscores');
+  new WOW().init();
   $('#carousel1').slick({
     dots: false,
     arrows: false,
     autoplay: true,
     fade: true,
     speed: 1000,
-    easing: 'easeOutQuint'
+    easing: 'easeOutQuint',
+    touchMove: false,
+    draggable: false,
+    swipe: false
   });
   $('#carousel2').slick({
     dots: false,
     arrows: false,
     autoplay: true,
     speed: 1000,
-    easing: 'easeOutQuint'
+    easing: 'easeOutQuint',
+    touchMove: false,
+    draggable: false,
+    swipe: false
   });
 };
